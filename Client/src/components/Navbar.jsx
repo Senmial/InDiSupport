db6483a154cab78b21ced8f8d6b80cf01e823da0
Sennie <senmial@gmail.com>
2024-04-18 21:57:47 +0100

    added gallery

--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 import logo from "../assets/images/logo.svg"
+import luckycharm from "../assets/images/luckycharm.svg"
 import SearchIcon from "../assets/images/searchIcon.svg"
 import { useNavigate } from "react-router-dom";
 
@@ -12,14 +13,29 @@ const Navbar = () => {
   }
 
   return (
-    <div className='backdrop-blur-xs p-1 h-24 px-14 py-5 '>
+    <div className='bg-[#FFFFFF] p-1 h-24 px-14 py-5 w-full fixed top-0'>
       <nav className='flex items-center justify-between px-5'>
         <div className='flex items-center' >
         <img src={logo} className="w-1/2 " alt="logo" onClick={() => goTo('/requesterhome')}/>
         </div>
       
+
+      
+<form class="w-1/4">   
+    <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+    <div class="relative">
+        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+            </svg>
+        </div>
+        <input type="search" id="default-search" class="block w-full p-4 ps-10  text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-[#41788E] blue-500 focus:border-[#41788E] dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-[#41788E] dark:focus:border-[#41788E]" placeholder="Search services, locations..." required />
+        <button type="submit" class="text-white absolute font-light end-2.5 bottom-2.5 bg-[#380445] hover:bg-[#F7A741] focus:ring-4 focus:outline-none focus:ring-[#41788E] rounded-lg text-sm px-4 py-2 dark:bg-[#41788E] dark:hover:[#41788E] dark:focus:ring-[#41788E]">Search</button>
+    </div>
+</form>
+
+
         <div className=' flex gap-5 font-thin text-md text-[#380445] '>
-          <img src={SearchIcon} alt="" />
         <Link className="">Accessibility</Link>
      
       <div className=' '>
@@ -42,6 +58,11 @@ const Navbar = () => {
     </div>
     </div>
       </nav>
+      <img
+                src={luckycharm}
+                className="absolute top-0 right-0"
+                alt=""
+              />
     </div>
   );
 };
--- Client/src/pages/MasbithCare.jsx
+++ Client/src/pages/MasbithCare.jsx
@@ -1,13 +1,20 @@
 import React from "react";
+
 import Navbar from "../components/Navbar.jsx";
 import masbithImg from "../assets/images/masbithImg.jpg";
 import disability1 from "../assets/images/disability1.jpg";
 import disability2 from "../assets/images/disability2.jpg";
 import disability4 from "../assets/images/disability4.jpg";
 import disability5 from "../assets/images/disability5.jpg";
+import disability6 from "../assets/images/disability6.jpg";
+import disability7 from "../assets/images/disability7.jpg";
+import disability8 from "../assets/images/disability8.jpg";
 import map from "../assets/images/map.jpg";
-import luckycharmTwo from "../assets/images/luckycharmTwo.svg";
-
+import faceBookIcon from "../assets/images/faceBookIcon.svg";
+import twitterIcon from "../assets/images/twitterIcon.svg";
+import instagramIcon from "../assets/images/instagramIcon.svg";
+import youtubeIcon from "../assets/images/youtubeIcon.svg";
+import Location from "../assets/images/location.svg";
 
 const MasbithCare = () => {
   return (
@@ -15,42 +22,41 @@ const MasbithCare = () => {
       <Navbar />
       <section className="flex p-20 gap-10 ">
         <div className="h-full w-3/4">
-          <img src={masbithImg} className="rounded-2xl " alt="" />
-        </div>
-        <article className="w-1/2 h-1/2 p-10 bg-[#380445] rounded-2xl ">
-          <img src={map} className=" " alt="" />
-            <button className=" text-[#F7A741]  px-2 hover:text-[#380445] focus:ring-2 focus:ring-[#380445]">
-              Get Directions
-            </button>
-          <div className=" rounded-md bg-[#FFFFFF] mt-5 p-3">
-          <img
-                src={luckycharmTwo}
-                className="absolute top-0 right-0"
-                alt=""
-              />
-          <p className="text-[#380445] text-2xl ">Masbith Care</p>
+          <div>          <p className="text-[#380445] text-3xl  mt-10 mb-5">Masbith Care</p>
+</div>
+          <img src={masbithImg} className="rounded-2xl" alt="" />
 
-            <p className=" items-center  text-[#380445]  font-thin text-lg">
-              356 Nnamdi Azikiwe Close, Wuse, Abuja, Nigeria{" "}
-            </p>
+        </div>
+        <article className="w-1/2 h-1/2 p-10 bg-[#380445] rounded-2xl mt-24 ">
+          <img src={map} className=" " alt="" />
+          
+          <div className=" rounded-md bg-[#FFFFFF] mt-5 p-3 flex gap-">
+            <img src={Location} alt="" />
+              <p className=" items-center  text-[#380445]   text-md">
+                356 Nnamdi Azikiwe Close, Wuse, Abuja, Nigeria{" "}
+              </p>
           </div>
+          <button className=" text-[#F7A741]  p-2 hover:text-[#41788E]">
+            Get Directions
+          </button>
           <br />
-          <div>
-          <div className="px-2 text-[#Ffffff] font-thin   ">
-            <button className="hover:text-[#F7A741] ">
-              Contact Us
-            </button>
-            <br />
-            <button className="hover:text-[#F7A741] ">
-              Visit Website
-            </button>
+          <div className="flex justify-between p-2 ">
+            <div className=" text-[#Ffffff] font-thin   ">
+              <button className="hover:text-[#F7A741] ">Contact Us</button>
+              <br />
+              <button className="hover:text-[#F7A741] ">Visit Website</button>
             </div>
-            {/* <div>
-            </div> */}
+            <div className="flex items-center gap-5 mt-3">
+              <img src={faceBookIcon} className="hover:outline-[#41788E]" alt="" />
+              <img src={twitterIcon} alt="" />
+              <img src={instagramIcon} alt="" />
+              <img src={youtubeIcon} alt="" />
             </div>
+          </div>
         </article>
       </section>
-      <section className="flex flex-nowrap p-10 gap-20 w-full ">
+
+      <section className="flex flex-nowrap p-5 w-full mt-0 ">
         <div className="  overflow-hidden border backdrop-blur-xl  ">
           <div className="p-5">
             <div className="flex flex-col gap-4 text-[#380445] font-light text-sm mt-4">
@@ -71,7 +77,8 @@ const MasbithCare = () => {
                   and support to empower our guests towards a brighter future.
                 </p>
                 <p>
-                  Masbith Care caters exclusively to <b>people living with disabilities.</b>
+                  Masbith Care caters exclusively to{" "}
+                  <b>people living with disabilities.</b>
                 </p>
               </div>
             </div>
@@ -79,14 +86,86 @@ const MasbithCare = () => {
           </div>
         </div>
       </section>
-      <section>
-        {/* <div>
-        <img src={disability1} className="w-1/5" alt="" />
-         <img src={disability2} className="w-1/5" alt="" />
-         <img src={disability4} className="w-1/5"  alt="" />
-         <img src={disability5} className="w-1/5" alt="" />
-        </div> */}
+
+      <section className="flex flex-col justify-center items-center gap-3">
+        <p className="text-[#380445]">Book an appointment</p>
+
+        <div>
+          {/* Pick a date  */}
+          <div className="relative max-w-sm">
+            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+              <svg
+                className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="#380445"
+                viewBox="0 0 20 20"
+              >
+                <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
+              </svg>
+            </div>
+            <input
+              type="date"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#380445] focus:border-[#380445] block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-[#380445] dark:focus:border-[#380445]"
+              placeholder="Select date"
+            />
+          </div>
+          {/* Pick a time */}
+          <div className="flex items-center gap-3 mt-3">
+            <input
+              type="time"
+              className="max-w-sm px-3 py-2 bg-gray-50  border border-gray-300 rounded-md focus:outline-none focus:ring-[#380445] focus:border-[#380445]"
+            />
+            <button className="bg-[#F7A741] hover:bg-[#380445] text-white font-thin text-sm py-2 px-4 rounded-full">
+              Select Time
+            </button>
+          </div>
+        </div>
       </section>
+
+      {/* 
+        <div className="flex  gap-5 p-5 overflow-x-scroll [&::-webkit-scrollbar]:hidden ">
+          <img src={disability2} className="w-1/5 h-1/2" alt="" />
+          <img src={disability5} className="w-1/5 h-1/2" alt="" />
+          <img src={disability1} className="w-1/5 h-1/2" alt="" />
+          <img src={disability4} className="w-1/5 h-1/2" alt="" />
+          <img src={disability6} className="w-1/5 h-1/2" alt="" />
+          <img src={disability7} className="w-1/5 h-1/2" alt="" />
+          <img src={disability8} className="w-1/5 h-1/2" alt="" />
+        </div>
+      */}
+
+
+      
+<section className="p-5">
+        <button className=" text-[#F7A741] text-xl  mt-5 mb-5 px-5 hover:text-[#380445]">
+          View Gallery and Events
+        </button>
+<div className="grid grid-cols-2 md:grid-cols-3 gap-3">
+    <div>
+        <img className="h-auto max-w-full rounded-lg" src={disability2} alt=""/>
+    </div>
+    <div>
+        <img className="h-auto max-w-full rounded-lg" src={disability5} alt=""/>
+    </div>
+    <div>
+        <img className="h-auto max-w-full rounded-lg" src={disability1} alt=""/>
+    </div>
+    <div>
+        <img className="h-auto max-w-full rounded-lg" src={disability8}alt=""/>
+    </div>
+    <div>
+        <img className="h-auto max-w-full rounded-lg" src={disability4} alt=""/>
+    </div>
+    <div>
+        <img className="h-auto max-w-full rounded-lg" src={disability6} alt=""/>
+    </div>
+    <div>
+        <img className="h-auto max-w-full rounded-lg" src={disability7} alt=""/>
+    </div>
+</div>
+</section> 
+
     </div>
   );
 };
