8f9c6c7244dce9e1d6c3bf6dd98c7624ac1c5854
Sennie <senmial@gmail.com>
2024-05-18 08:03:12 +0100

    adding media query

--- Client/src/pages/LandingPage.jsx
+++ Client/src/pages/LandingPage.jsx
@@ -10,28 +10,34 @@ const Home = () => {
   };
   return (
     <div className="">
-        <img src={bgImg} className="w-full h-screen object-cover absolute " alt="" />
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-center p-10 rounded-md w-2/4 backdrop-blur-xl ">
-            <img src={volunteer4} className="w-3/4 " alt="" />
-            <p className=" text-[#FFFFFF] font-extralight">
-              Lending a hand today, goes a long way tomorrow.
-            </p>
-            <p className="pt-10 text-center text-md font-thin text-[#FFFFFF]">
-              InDiSupport, a non-governmental organization (NGO), is dedicated
-              to addressing the urgent needs of internally displaced Nigerians through the helping hands of volunteers.
-            </p>
-            <div className="flex justify-center items-center mt-5 pt-10 ">
-              
-             
-              <button
-                className="rounded-full text-[#FFFFFF] p-4 bg-[#380445] drop-shadow-md w-40 hover:bg-[#F7A741] focus:ring-2 focus:ring-[#380445]"
-                onClick={() => goTo("/login")}
-              >
-                Proceed
-              </button>
-            </div>
-          </div> 
+      <img
+        src={bgImg}
+        className="w-full h-screen object-cover absolute "
+        alt=""
+      />
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-center p-5 sm:p-10 rounded-md w-3/4 sm:w-2/4 backdrop-blur-xl ">
+        <img src={volunteer4} className="w-3/4 md:w-2/4 " alt="" />
+        <p className=" text-[#FFFFFF] font-extralight mt-5 text-sm sm:text-md ">
+          Lending a hand today, goes a long way tomorrow.
+        </p>
+        <p className="pt-10 text-center text-sm sm:text-md font-thin text-[#FFFFFF]">
+          InDiSupport, a non-governmental organization (NGO), is dedicated to
+          addressing the urgent needs of internally displaced Nigerians through
+          the helping hands of volunteers.
+        </p>
+        
+          <a href="/about" className="my-5 underline">Learn More</a>
+        
+        <div className="flex justify-center items-center mt-10 ">
+          <button
+            className="rounded-full text-[#FFFFFF] p-4 bg-[#380445] drop-shadow-md w-32 sm:w-40 hover:bg-[#F7A741] focus:ring-2 focus:ring-[#380445]"
+            onClick={() => goTo("/login")}
+          >
+            Proceed
+          </button>
         </div>
+      </div>
+    </div>
   );
 };
 
